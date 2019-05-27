9c580140e1f5377a3fcda004cf4ba823dbe8951b
Tiago Bento <tiagobentofernandes@gmail.com>
2019-05-27 14:31:28 -0300

    Revert "uberfire-security-client-backend"
    This reverts commit d2bb756010f3e64d080fa2d86fee2306de2a898e.

--- uberfire-security/uberfire-security-client/src/test/java/org/uberfire/security/client/SecurityEntryPointTest.java
+++ uberfire-security/uberfire-security-client/src/test/java/org/uberfire/security/client/SecurityEntryPointTest.java
@@ -1,11 +1,11 @@
 /*
- * Copyright 2019 Red Hat, Inc. and/or its affiliates.
+ * Copyright 2017 Red Hat, Inc. and/or its affiliates.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *        http://www.apache.org/licenses/LICENSE-2.0
+ *       http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -14,7 +14,9 @@
  * limitations under the License.
  */
 
-package org.uberfire.security.client;import org.junit.Test;
+package org.uberfire.security.client;
+
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
@@ -22,7 +24,6 @@ import org.mockito.runners.MockitoJUnitRunner;
 import org.uberfire.backend.events.AuthorizationPolicySavedEvent;
 import org.uberfire.security.authz.AuthorizationPolicy;
 import org.uberfire.security.authz.PermissionManager;
-import org.uberfire.security.client.SecurityEntryPoint;
 
 import static org.mockito.Mockito.*;
 
